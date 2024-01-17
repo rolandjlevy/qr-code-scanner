7987d4924c1e74b42643a5261998cabaf90a4144
Roland Levy <contact@rolandlevy.co.uk>
2024-01-17 23:40:55 +0000

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# qr-code-scanner
+# QuickScan - QR Code Scanner
+
+### View the project in VS Code Simple browser
+
+- Get the URL for the page - see PORTS in terminal
+- From command palette: Simple Browser: Show
+- then paste in that URL
+
+### To run a front-end project in a Codespace
+
+- Terminal > npm i -g http-server
+- Terminal > http-server
+- click 'Open in Browser'
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+(async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: {
+        ideal: 'environment'
+      }
+    },
+    audio: false
+  });
 
+  const videoEl = document.querySelector('#stream');
+  videoEl.srcObject = stream;
+  await videoEl.play();
+
+  const barcodeDetector = new BarcodeDetector({
+    formats: ['qr_code', 'code_39', 'codabar', 'ean_13']
+  });
+
+  window.setInterval(async () => {
+    const barcodes = await barcodeDetector.detect(videoEl);
+    if (barcodes.length <= 0) return;
+    const resultEl = document.querySelector('#result');
+    const scannedData = barcodes.map((barcode) => barcode.rawValue);
+    console.log({ scannedData });
+    resultEl.textContent = scannedData;
+  }, 1000);
+})();
